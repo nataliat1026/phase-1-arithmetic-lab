8ecd7c9d08b4e773fe1aa5689e3f5a926801ac6b
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-04 20:10:17 -0400

    commit tree 6e1e7d66abc2f03adae4d492c356670c162a0e9f

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+multiply;
+
+let random = Math.ceil(Math.random() * 10);
+
+random;
+
+
+const num3 = 29;
+const num4 = 5;
+
+let mod = num3 % num4;
+
+
+let max = Math.max(5, 10, 15, 20);
+
+max;
